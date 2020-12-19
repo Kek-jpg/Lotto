7bd46b6d30f1d5d7c7ca595d9fd7fc21e5a2f529
Kek-jpg <Kek-jpg@users.noreply.github.com>
2020-12-19 21:32:25 +0500

    Написана работающая версия кода

--- Lotto.py
+++ Lotto.py
@@ -1,1 +1,13 @@
 
+import random
+
+N = int(input("Введите N: "))
+pulled_out_barrels = []
+
+while len(pulled_out_barrels) < N:
+    barrel = random.randint(1, N)
+    if barrel not in pulled_out_barrels:
+        pulled_out_barrels.append(barrel)
+        tap = str(input("Введите что угодно и получите случайный бочонок ! "))
+        print("Вытащен бочонок номер " + str(barrel) + " !")
+print("Бочонки кончились, жеребьёвка завершена !")
